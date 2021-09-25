da4a2594d5dca6941fe07c4bf383b38fadb0c0ce
yanglichao <ylc2007.good@163.com>
2021-09-25 07:45:22 +0000

    defalutes.py add check_app() check_Installed()

---
+++ README.md
@@ -0,0 +1,16 @@
+## simple_data项目
+    - 利用python、sqlite3、flask构建数据可视化
+## simple_data目录
+    - /db  数据库  [git排除]
+    - /download 下载目录
+    - /flask  前端框架
+    - /operat  数据库数据在整理
+    - /strategy 存储多种策略
+    - /tools  工具目录
+    - README.md  说明文档
+    - .gitignore  Git配置
+    - config.ini  初始化文件  [git排除]
+    - defaults.py  defaults目录主模块
+    - download.py  download目录主模块
+    - operat.py  operat目录主模块
+    - strategy.py  strategy目录主模块
---
+++ defaults.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python3
+"""
+检测: pip 有无安装
+检测: 需要三方库 有无安装
+初始化参数
+
+"""
+import os,re
+
+APP = 'pip'  # Linux需要有pip安装
+URL = r'https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple/'  #清华镜像网站
+# 第三方库
+LIBS = ['pandas',\
+		'zmail',\
+		"akshare",\
+		"baostock",\
+		"configparser"\
+		]
+
+def check_app():
+	res = os.popen("{0} -V".format(APP)).read()
+	if res != '':  # 成功安装
+		pass
+	else:
+		raise Exception("{0} 没有安装\n请先安装 {0}".format(APP))
+
+def _sep(s):
+	# s --> 'pandas                            1.4.4'
+    s = re.sub(r'\s+',' ', s).strip()
+    return s.split(' ')
+
+def _getDict(command = APP):
+    alllib = os.popen(f"{command} list").read()  # str
+    alist = alllib.splitlines()  # list
+    assert len(alist) > 2, alllib  # 断言如果不满足print alllib
+    alist = alist.__iter__()  # alist生成迭代器liter()也可以
+    assert next(alist).startswith('Package'), alllib  # 跳过第一行str Package
+    assert next(alist).startswith('---'), alllib  # 跳过第二行str  ---
+    # map(_sep, alist)
+    # [ ['appdirs', '1.4.4'],['akshare', '0.8.70'], ... ,['astroid', '2.5.6'] ]
+    # 嵌套列表转换dic
+    # https://www.pythonf.cn/read/61002#2.dict%E8%BD%ACList
+    return dict(map(_sep, alist))
+
+def check_Installed(LIBS):
+    libdict = _getDict()
+    for lib in LIBS:
+        if lib not in libdict:  # libdict.keys()也可以
+                os.system('sudo %s install %s -i %s'%(APP,lib,URL))
+        else:
+        	pass
+        	# print(f"{lib}第三方库已经安装")
+
+def main():
+	# 1. pip 有无安装
+	check_app()
+	# 2. 第三方库检测没有安装
+	check_Installed(LIBS)
+	# 3. TODO configparser 初始化参数
+if __name__ == "__main__":
+	main()
