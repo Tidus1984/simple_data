b40dc34958ed36e200a000d5ec16bd0c5b4597fd
yanglichao <ylc2007.good@163.com>
2021-10-19 08:06:59 +0000

    download folder add baostock_sz50.py baostock_zz500.py

--- download/baostock_sz50.py
+++ download/baostock_sz50.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3
 """
-作用: 1. 每日baostock接口更新沪深300组成成分
-      2. 存入数据库./db/stock.db BAOSTOCK_HS300表中
+作用: 1. 每日baostock接口更新上证50组成成分
+      2. 存入数据库./db/stock.db BAOSTOCK_SZ50表中
 用法:
-- baostock_hs300_update()
-    - 每日更新沪深300组成成分从baostock
-    - updateDate    |   code    |      code_name
-        2018-11-26 	  sh.600000 	浦发银行
+- baostock_sz50_update()
+    - 每日更新上证50组成成分从baostock
+    - updateDate    |   code    |   code_name
+        2021-10-18     sh.603986     兆易创新
+
 """
 from simple_data.app import defaults
 import configparser,os,sqlite3
@@ -29,17 +30,17 @@ def is_db_table(tb_name):
     except:
         return False
 
-def baostock_hs300_update(sql_table_name = "BAOSTOCK_HS300"):
-    # 从baostock下载沪深300成分
+def baostock_sz50_update(sql_table_name = "BAOSTOCK_SZ50"):
+    # 从baostock下载上证50成分
     lg = bs.login()  # 登陆
-    rs = bs.query_hs300_stocks()
-    hs300_stocks = []
+    rs = bs.query_sz50_stocks()
+    sz50_stocks = []
     while (rs.error_code == '0') & rs.next():
         # 获取一条记录，将记录合并在一起
-        hs300_stocks.append(rs.get_row_data())
-    df = pd.DataFrame(hs300_stocks, columns=rs.fields)
+        sz50_stocks.append(rs.get_row_data())
+    df = pd.DataFrame(sz50_stocks, columns=rs.fields)
     conn = sqlite3.connect(DB_NAME)
-    # 如果没有 BAOSTOCK_HS300 tables
+    # 如果没有 BAOSTOCK_SZ50 tables
     if not is_db_table(sql_table_name):
         df.to_sql(sql_table_name, conn, if_exists="replace",index=False)
         print(f"{os.path.basename(__file__)}模块初始化:\n{DB_NAME}数据库-->{sql_table_name}表")
@@ -51,12 +52,13 @@ def baostock_hs300_update(sql_table_name = "BAOSTOCK_HS300"):
         if rs == []:
             pass  # 完全一样不用更新
             # print(f"{os.path.abspath(__file__)}模块不需要更新")
-        else:  # hs300数据表更新
-            if df.shape[0] == 300:
+        else:  # sz50数据表更新
+            if df.shape[0] == 50:
                 df.to_sql(sql_table_name, conn, if_exists="replace",index=False)
+                # print(f"{sql_table_name}更新")
             else:
                 print(df)
                 raise Exception(f"{os.path.abspath(__file__)}模块: baostock下载数据错误")
 
 if __name__ == "__main__":
-    baostock_hs300_update()
+    baostock_sz50_update()
--- download/baostock_zz500.py
+++ download/baostock_zz500.py
@@ -1,12 +1,12 @@
 #!/usr/local/bin/python3
 """
-作用: 1. 每日baostock接口更新沪深300组成成分
-      2. 存入数据库./db/stock.db BAOSTOCK_HS300表中
+作用: 1. 每日baostock接口更新中证500组成成分
+      2. 存入数据库./db/stock.db BAOSTOCK_ZZ500表中
 用法:
-- baostock_hs300_update()
-    - 每日更新沪深300组成成分从baostock
-    - updateDate    |   code    |      code_name
-        2018-11-26 	  sh.600000 	浦发银行
+- baostock_zz500_update()
+    - 每日更新中证500组成成分从baostock
+    - updateDate    |   code    |   code_name
+        2021-10-18    sz.300618     寒锐钴
 """
 from simple_data.app import defaults
 import configparser,os,sqlite3
@@ -29,17 +29,17 @@ def is_db_table(tb_name):
     except:
         return False
 
-def baostock_hs300_update(sql_table_name = "BAOSTOCK_HS300"):
-    # 从baostock下载沪深300成分
+def baostock_zz500_update(sql_table_name = "BAOSTOCK_ZZ500"):
+    # 从baostock下载中证500成分
     lg = bs.login()  # 登陆
-    rs = bs.query_hs300_stocks()
-    hs300_stocks = []
+    rs = bs.query_zz500_stocks()
+    zz500_stocks = []
     while (rs.error_code == '0') & rs.next():
         # 获取一条记录，将记录合并在一起
-        hs300_stocks.append(rs.get_row_data())
-    df = pd.DataFrame(hs300_stocks, columns=rs.fields)
+        zz500_stocks.append(rs.get_row_data())
+    df = pd.DataFrame(zz500_stocks, columns=rs.fields)
     conn = sqlite3.connect(DB_NAME)
-    # 如果没有 BAOSTOCK_HS300 tables
+    # 如果没有 BAOSTOCK_ZZ500 tables
     if not is_db_table(sql_table_name):
         df.to_sql(sql_table_name, conn, if_exists="replace",index=False)
         print(f"{os.path.basename(__file__)}模块初始化:\n{DB_NAME}数据库-->{sql_table_name}表")
@@ -51,12 +51,13 @@ def baostock_hs300_update(sql_table_name = "BAOSTOCK_HS300"):
         if rs == []:
             pass  # 完全一样不用更新
             # print(f"{os.path.abspath(__file__)}模块不需要更新")
-        else:  # hs300数据表更新
-            if df.shape[0] == 300:
+        else:  # zz500数据表更新
+            if df.shape[0] == 500:
                 df.to_sql(sql_table_name, conn, if_exists="replace",index=False)
+                # print(f"{sql_table_name}更新")
             else:
                 print(df)
                 raise Exception(f"{os.path.abspath(__file__)}模块: baostock下载数据错误")
 
 if __name__ == "__main__":
-    baostock_hs300_update()
+    baostock_zz500_update()
